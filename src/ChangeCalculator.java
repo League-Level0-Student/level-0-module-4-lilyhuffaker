3bda7e6f7428db0362e166d88bc6c471ef2bcf55
Huffaker.Lily <Huffaker.Lily@SEMS-259-32.SEMS259.LAB>
2018-03-28 16:31:36 -0700

    happy birthday

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,26 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String Nickles=JOptionPane.showInputDialog("Dr. Nugget: How many Nickels wich are a type of coin currency does one aqui- \n Ralph: Get to the point!"
+		+ " Dr. Nugget: Right, how many \n nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+int cents = Integer.parseInt(Nickles);
 		// Ask the user how many dimes they have, and convert their answer
-
+String Dimes=JOptionPane.showInputDialog("Ralph: Make it quick this time! Dr. Nugget: Ok, Ok! How many dimes does thy have? \n there that better!?");
+int centstwo = Integer.parseInt(Dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+String Quarters = JOptionPane.showInputDialog("Dr. Nugget: How many quarters do you have?");
+int centsthree = Integer.parseInt(Quarters);
+String Dollars = JOptionPane.showInputDialog("Ralph: I swear you need to say this with nothing extra or facy! Dr.Nugget: Ok Ok, how many dollars do you have?");
+int papper = Integer.parseInt(Dollars);
 		// Calculate how much money the user has and save it in a double variable 
-
+double total = 0.00;
+total+= 0.05*cents;
+total+= 0.10*centstwo;
+total+= 0.25*centsthree;
+total+= 1.00*papper;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "Dr. Nugget: You, in total, in the end, you have, "+total+" money.");
 	}
 }
 
--- src/HappyPet.java
+++ src/HappyPet.java
@@ -4,20 +4,28 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+static int happinessLevel = 0;	
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+String Pet = JOptionPane.showInputDialog("Dr.Nugget: hello, welcome, what kind of pet would you like?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "Dr. Nugget: What would you like to do for your pet?", "Pet Care", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Cuddle", "Feed", "Jog With" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+if(task==0) {
+	Cuddle(Pet);
+}
+if(task==1) {
+	Feed(Pet);
+}
+if(task==2) {
+	JogWith(Pet);
+}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
@@ -26,4 +34,37 @@ public class HappyPet {
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+static void Cuddle(String Pet) {
+	if(Pet.equals("cat")) {
+		JOptionPane.showMessageDialog(null, Pet+" Seems very happy and it purrs.");
+		happinessLevel+=2;
+	}
+	if(Pet.equals("dog")) {
+		JOptionPane.showMessageDialog(null, Pet+" Seems very happy and it licks your face.");
+		happinessLevel+=4;	
+	}
+	else{
+		JOptionPane.showMessageDialog(null, Pet+" allows the cuddle to happen with no emotion");
+		happinessLevel++;
+	}
 }
+static void Feed(String Pet) {
+		JOptionPane.showMessageDialog(null, Pet+" Noms food happily");
+		happinessLevel+=2;
+	}
+static void JogWith(String Pet) {
+	if(Pet.equals("cat")) {
+		JOptionPane.showMessageDialog(null, Pet+" Poutrs on the sidewalk and doesnot move.");
+		happinessLevel--;
+	}
+	if(Pet.equals("dog")) {
+		JOptionPane.showMessageDialog(null, Pet+" Seems very happy and it wags its tail the whole time.");
+		happinessLevel+=5;
+	}
+	if(Pet.equals("snake")) {
+		JOptionPane.showMessageDialog(null, Pet+"Much happy, very snake.");
+		happinessLevel++;
+	}
+}
+}
+	
